59c0e4afaa18d058299e0d05ff515f383e108c80
Deepak Vig <deepakvig@gmail.com>
2013-03-28 19:16:03 +0530

    specs added for index methos

--- spec/controllers/files_controller_spec.rb
+++ spec/controllers/files_controller_spec.rb
@@ -2,4 +2,18 @@ require 'spec_helper'
 
 describe FilesController do
 
+  describe 'GET #index' do
+    it "populates an array of files" do
+      get :index
+      expect(assigns(:files)).not_to be_nil
+    end
+
+    it "renders the :index view" do
+      get :index
+      response.should render_template :index
+      expect(response).to render_template :index
+    end
+
+  end
+
 end
